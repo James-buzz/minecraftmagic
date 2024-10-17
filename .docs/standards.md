27d98b70fc2d10b60e105392d2556854f53588bd
james.lomax <james.lomax@thecyberhawk.com>
2024-10-17 15:25:12 +0100

    temporary test

--- .docs/standards.md
+++ .docs/standards.md
@@ -3,3 +3,4 @@
 - service classes can throw exceptions if an unexpected scenario occurs with a custom exception. 
 - presentation layer (controller) will handle exceptions and return a proper response to the client.
 
+- repositories are easy to mock rather than models in unit tests. 
---
+++ tests/Feature/Http/Controllers/WelcomeController/BaseWelcomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Http\Controllers\WelcomeController;
+
+use PHPUnit\Framework\Attributes\Large;
+use Tests\Feature\FeatureTestCase;
+
+/**
+ * @group WelcomeController
+ */
+#[Large] class BaseWelcomeController extends FeatureTestCase {}

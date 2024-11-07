70cde40a89b3155865c4d7ffa6748e2b6bc3a151
James <jameslomax96@gmail.com>
2024-11-07 21:48:53 +0000

    Add types

--- app/Concerns/HasFeedback.php
+++ app/Concerns/HasFeedback.php
@@ -3,21 +3,27 @@
 namespace App\Concerns;
 
 use App\Models\Feedback;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait HasFeedback
 {
     /**
      * Get all feedback for this model.
+     *
+     * @return MorphMany<Feedback, Model>
      */
-    public function feedback()
+    public function feedback(): \Illuminate\Database\Eloquent\Relations\MorphMany
     {
         return $this->morphMany(Feedback::class, 'feedbackable');
     }
 
     /**
      * Add feedback to this model.
+     *
+     * @param array<string, mixed> $data
      */
-    public function addFeedback(array $data)
+    public function addFeedback(array $data): Feedback
     {
         return $this->feedback()->create(array_merge($data, [
             'user_id' => auth()->id(),

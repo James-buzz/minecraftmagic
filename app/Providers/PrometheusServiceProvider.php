c60621795a76234d4f3999141ae5611b64be0186
james.lomax <james.lomax@thecyberhawk.com>
2024-11-08 12:52:07 +0000

    Add redis back

--- app/Providers/PrometheusServiceProvider.php
+++ app/Providers/PrometheusServiceProvider.php
@@ -2,18 +2,29 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 use Prometheus\CollectorRegistry;
 use Prometheus\Storage\InMemory;
+use Prometheus\Storage\Redis;
 
 class PrometheusServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(CollectorRegistry::class, function () {
-            // TODO: Use a real database connection
-            // TODO: temp this wont be used
-            return new CollectorRegistry(new InMemory);
+            if (app()->environment('testing')) {
+                return new CollectorRegistry(new InMemory);
+            }
+
+            Redis::setDefaultOptions(
+                Arr::only(
+                    config('database.redis.default'),
+                    ['host', 'port', 'password']
+                )
+            );
+
+            return CollectorRegistry::getDefault();
         });
     }
 }

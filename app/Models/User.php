9a985348918c11cf48e83760d8afad0d4ae3fbfd
James <jameslomax96@gmail.com>
2024-11-09 09:57:52 +0000

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,7 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $fillable = [
         'name',
         'email',
+        'email_verified_at',
         'password',
         'provider_id',
         'provider_type',

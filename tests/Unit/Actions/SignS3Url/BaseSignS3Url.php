4266c1fc26037dcc4b93f33189771963e969c411
James <jameslomax96@gmail.com>
2024-11-09 16:44:50 +0000

    Fix issue

--- tests/Unit/Actions/SignS3Url/BaseSignS3Url.php
+++ tests/Unit/Actions/SignS3Url/BaseSignS3Url.php
@@ -2,17 +2,17 @@
 
 namespace Tests\Unit\Actions\SignS3Url;
 
-use App\Actions\SignS3Url;
+use App\Actions\SignS3URL;
 use Tests\TestCase;
 
 class BaseSignS3Url extends TestCase
 {
-    protected SignS3Url $action;
+    protected SignS3URL $action;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->action = new SignS3Url;
+        $this->action = new SignS3URL;
     }
 }
